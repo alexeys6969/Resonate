b6e62c0ab3a6547b9133a57855178daa3999c1e0
Алексей Шашин <alexshashin.2023@gmail.com>
2026-03-12 16:32:53 +0500

    Полностью(почти) разработанный интерфейс приложения

---
+++ Pages/Supply/Elements/NewProductItem.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Resonate.Pages.Sales.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для NewProductItem.xaml
+    /// </summary>
+    public partial class NewProductItem : UserControl
+    {
+        Pages.Supply.Add add;
+        public NewProductItem(Add add)
+        {
+            InitializeComponent();
+            this.add = add;
+        }
+
+        private void DeleteProduct(object sender, RoutedEventArgs e)
+        {
+            add.NewProductParent.Children.Remove(this);
+        }
+    }
+}
