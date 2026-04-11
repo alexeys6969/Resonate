d2079b3a1885c5068ab3248b231146dc875c0543
Алексей Шашин <alexshashin.2023@gmail.com>
2026-04-11 15:01:56 +0500

    Добавлена иконка приложений, создана верхняя панель управления, написана документация к API

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Net.Http;
 
 namespace Resonate
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public static MainWindow init;
@@ -28,7 +13,54 @@ namespace Resonate
             InitializeComponent();
             init = this;
             frame.Navigate(new Pages.Authorization());
+        }
 
+        // Перетаскивание окна
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                // Двойной клик — развернуть/восстановить
+                Maximize_Click(null, null);
+            }
+            else
+            {
+                DragMove();
+            }
+        }
+
+        // Свернуть
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+            var anim = new System.Windows.Media.Animation.DoubleAnimation(0, TimeSpan.FromMilliseconds(150));
+            anim.Completed += (s, a) => WindowState = WindowState.Minimized;
+            BeginAnimation(OpacityProperty, anim);
+        }
+
+        // Развернуть / Восстановить
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
+        }
+
+        // Плавное закрытие с анимацией (по желанию)
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            // Можно добавить плавное затухание
+            var fadeOut = new System.Windows.Media.Animation.DoubleAnimation
+            {
+                From = 1,
+                To = 0,
+                Duration = new Duration(TimeSpan.FromMilliseconds(180))
+            };
+
+            fadeOut.Completed += (s, a) => Close();
+
+            BeginAnimation(OpacityProperty, fadeOut);
         }
     }
 }
